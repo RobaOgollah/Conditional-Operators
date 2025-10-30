c837701b269a7ad8cf1a667af9d94a38240f129e
SuperHuman <git@github.com:RobaOgollah/Mortage-Calculator-.git>
2025-10-30 16:19:24 +0300

    first commit

---
+++ src/Main.java
@@ -0,0 +1,17 @@
+public class Main{
+    public static void main() {
+        int temp=32;
+        if(temp>30) {
+
+            System.out.println("It's a hot day");
+            System.out.println("Drink plenty of water");
+        }
+            else if(temp>20) {
+            System.out.println("It's a nice day");
+        }
+            else {
+            System.out.println("It's a cold day");
+
+        }
+    }
+}
